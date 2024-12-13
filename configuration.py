a279202a15e33b12afa5d8b53c936119ddd84fc0
Andrea S <m.andrea.salcedo@outlook.com>
2024-12-12 20:22:14 -0600

    Correcciones parte 2

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-79bf9395-3791-41fd-a160-e7f95dd3e34d.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = "https://cnt-4fd9fc96-e099-4c1f-a981-3a40a95f2ea1.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH = "/api/v1/users/" # Almacena la ruta para crear un usuario en esta variable
 KITS_PATH = "/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -5,7 +5,6 @@ import data
 import sender_stand_request
 from data import one_letter, no_letter, letter_max_limit, letter_limit_exceeded, special_character, string_with_numbers, \
     number_parameter, with_spaces, valid_kit_name
-from sender_stand_request import response_new_kit
 
 
 # El único campo que requiere cambios en todas las comprobaciones es "name"
@@ -23,22 +22,20 @@ def positive_assert(kit_name):
     kit_response = requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,
                 json = kit_body,
                 headers = data.headers)
-    return kit_response
+    assert kit_response.status_code == 201
+    assert kit_response.json()["name"] == valid_kit_name
+
     # Comprobar el código de estado de la respuesta y campo name
 def test_create_kit_with_valid_name():
-    response_create_kit = positive_assert(valid_kit_name)
-    assert response_create_kit.status_code == 201
-    assert response_create_kit.json()["name"] == valid_kit_name
+    positive_assert(valid_kit_name)
 
 # 1. El número permitido de caracteres en el nombre del kit es 1
 def test_create_kit_1_letter_in_name_get_success_response():
     positive_assert(one_letter)
-    assert response_new_kit.status_code == 201 # Comprobar el código de estado de la respuesta
 
 # 2. El número permitido de caracteres (511) en name
 def test_create_kit_511_letter_in_name_get_success_response():
     positive_assert(letter_max_limit)
-    assert response_new_kit.status_code == 201 # Comprobar el código de estado de la respuesta
 
 # 3. El campo name es un string vacío
 def negative_assert_empty_string(model):
@@ -54,37 +51,31 @@ def test_create_kit_empty_string_get_error_response():
 
 # 4. El número de caracteres es mayor que la cantidad permitida (512) en name
 def negative_assert(model):
-    response_limit_exceeded = requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,
+    kit_response = requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,
                 json = model,
                 headers = data.headers)
-    return response_limit_exceeded
+    assert kit_response.status_code == 400
 
 def test_create_kit_512_letter_in_name_get_error_response():
     kit_body = get_kit_body(letter_limit_exceeded)
-    response_limit_exceeded = negative_assert(kit_body)
-    assert response_limit_exceeded.status_code == 400 # Comprobar el código de estado de la respuesta
+    negative_assert(kit_body)
 
 # 5. Se permiten caracteres especiales en name
 def test_create_kit_symbol_in_name_get_success_response():
     positive_assert(special_character)
-    assert response_new_kit.status_code == 201 # Comprobar el código de estado de la respuesta
 
 # 6. Se permiten espacios en name
 def test_create_kit_space_in_name_get_success_response():
     positive_assert(with_spaces)
-    assert response_new_kit.status_code == 201 # Comprobar el código de estado de la respuesta
 
 # 7. Se permiten números en name
 def test_create_kit_numbers_in_name_get_success_response():
     positive_assert(string_with_numbers)
-    assert response_new_kit.status_code == 201 # Comprobar el código de estado de la respuesta
 
-# 8. Falta el parámetro name no se pasa en la solicitud
+# 8. Falta el parámetro name, no se pasa en la solicitud
 def test_create_kit_no_name_get_error_response():
-    kit_body = data.kit_body.copy() # Se copia el diccionario con el cuerpo de la solicitud de data a kit_body
-    kit_body.pop("name") # El parámetro name se elimina de la solicitud
-    negative_assert(kit_body) # Comprueba la respuesta
-    assert response_new_kit.status_code == 400 # Comprobar el código de estado de la respuesta
+    kit_body = get_kit_body(None)
+    negative_assert(kit_body)
 
 # 9. Se ha pasado un tipo de parámetro diferente en name (número)
 def test_create_kit_number_get_error_response():
